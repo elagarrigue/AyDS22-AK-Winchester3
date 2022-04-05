534b3b87a3c4c2c54acaac0679743d0613a692ac
Camila Arena <56278138+CamilaArena@users.noreply.github.com>
2022-04-05 15:47:50 -0300

    Agregue "releaseDatePresition" al modelo y elimine la propiedad "year"

--- app/src/main/java/ayds/winchester/songinfo/home/model/entities/SpotifySong.kt
+++ app/src/main/java/ayds/winchester/songinfo/home/model/entities/SpotifySong.kt
@@ -5,6 +5,7 @@ interface Song {
     val songName: String
     val artistName: String
     val albumName: String
+    val releaseDatePrecision: String
     val releaseDate: String
     val spotifyUrl: String
     val imageUrl: String
@@ -16,20 +17,19 @@ data class SpotifySong(
   override val songName: String,
   override val artistName: String,
   override val albumName: String,
+  override val releaseDatePrecision: String,
   override val releaseDate: String,
   override val spotifyUrl: String,
   override val imageUrl: String,
   override var isLocallyStored: Boolean = false
-) : Song {
-
-    val year: String = releaseDate.split("-").first()
-}
+) : Song
 
 object EmptySong : Song {
     override val id: String = ""
     override val songName: String = ""
     override val artistName: String = ""
     override val albumName: String = ""
+    override val releaseDatePrecision: String = ""
     override val releaseDate: String = ""
     override val spotifyUrl: String = ""
     override val imageUrl: String = ""
